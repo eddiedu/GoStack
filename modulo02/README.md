f2b8f1474bbc2653603d1dfea581be2aaaa6b56a
Eduardo Fagundes <eddiedu@gmail.com>
2020-01-28 17:45:04 -0300

    create project strucutre

---
+++ modulo02/README.md
@@ -0,0 +1,6 @@
+Dependencies
+
+Express - server http
+Sucrase - para trazer novas funcionalidades
+    ex: import express from 'express'
+Nodemon - automatic refresh
---
+++ modulo02/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+    constructor() {
+        this.server = express();
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.server.use(express.json());
+    }
+
+    routes() {
+        this.server.use(routes);
+    }
+}
+
+//Como as configurações iniciais não devem mais ser executadas
+//só o server vai ser exposto
+export default new App().server;
